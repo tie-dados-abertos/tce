cd491aff1dad27141a82f38f4cb50b091c0e3f94
Eudes <jadieleudes@gmail.com>
2019-02-21 13:23:54 -0300

    Converter Insert

---
+++ ConversaoInsertMySQL.py
@@ -0,0 +1,19 @@
+import os
+
+script_path = "C:/Users/Eudes/Documents/scriptTce"
+
+def percorrePastaRetornaListaSQL(caminho):
+    if not os.path.exists(script_path):
+        return "Este caminho não Existe."
+    else:
+        for diretorio,pasta,listaArquivo in os.walk(caminho):
+            return listaArquivo
+
+def converteScript(listaScript):
+    for script in listaScript:
+        arq = open(script, "r+b")
+        lista = arq.readlines()
+        print(lista)
+
+
+converteScript(percorrePastaRetornaListaSQL(script_path))
